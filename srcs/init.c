d7043da2620d620cbe6aed23746dfeba17071eb0
Theoli <theoli231@gmail.com>
2023-09-27 08:53:02 +0200

    Protected malloc

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: tlivroze <tlivroze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 00:03:10 by tlivroze          #+#    #+#             */
-/*   Updated: 2023/09/27 01:58:15 by tlivroze         ###   ########.fr       */
+/*   Updated: 2023/09/27 08:52:29 by tlivroze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,11 @@ int	init(t_data *data, int ac, char **av)
 	if (init_data(data, ac, av))
 		return (1);
 	data->philo = malloc(sizeof(t_philo) * data->nb_philo);
+	if (!(data->philo))
+		return (1);
 	data->forks = malloc(sizeof(pthread_mutex_t) * data->nb_philo);
+	if (!(data->forks))
+		return (free(data->philo), 1);
 	if (init_mutex(data))
 		return (free(data->philo), free(data->forks), 1);
 	if (init_philo(data))
